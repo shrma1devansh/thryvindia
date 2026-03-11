e05e4f6281511fea30c3476c6e139997002e6c06
Rishi <shrmadevansh007@gmail.com>
2026-03-11 17:36:49 +0530

    Thridcommit

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://thryvindia.onrender.com/api/waitlist",
 });
 
 export default API;
